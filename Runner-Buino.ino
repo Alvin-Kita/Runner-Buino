e4c1ba1e5a1917d8e337221d342a8233e451e9c2
jck <jckleinbourg@gmail.com>
2024-05-14 16:29:58 +0200

    Déclare variables sprites et struct Player

--- Runner-Buino.ino
+++ Runner-Buino.ino
@@ -1,10 +1,33 @@
 #include <Gamebuino-Meta.h>   
+#include "sprites.h"
 
 int _state = 1; // TODO: le repasser à 0
+
+// déclaration des sprites
+Image _spriteHero1(IMAGE_DATA_HERO1);
+Image _spriteHero2(IMAGE_DATA_HERO2);
+Image _spriteCactusLarge(IMAGE_DATA_CACTUS_LG);
+Image _spriteCactusSmall(IMAGE_DATA_CACTUS_SM);
+Image _spriteForeground(IMAGE_DATA_FOREGROUND);
+Image _spriteHeart(IMAGE_DATA_HEART);
+Image _spriteHole(IMAGE_DATA_HOLE);
+Image _spriteSky(IMAGE_DATA_CACTUS_SKY);
+
 
 // Valeur max de l'écran 80x64
 const int X_SCREEN_MAX = 78;
 const int Y_SCREEN_MAX = 62;
+
+struct Player {
+  float x;
+  float y;
+  int score;
+  int life;
+  int timer;
+  bool isJumping;
+  bool isDoubleJumping;
+  bool isCooldown;
+};
 
 
 void setup() {
