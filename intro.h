db6d9855f4ca856d40e287775c022a3b04ab9ba9
Almosk <alvin.kita@outlook.fr>
2024-05-15 11:49:43 +0200

    _playerTimerUpdate devient _playerSpriteUpdate avec ajout de la gestion d'affichage du sprite

--- intro.h
+++ intro.h
@@ -5,7 +5,7 @@ void introInit() {
 
 void introUpdate() {
 	
-	_playerTimerUpdate();
+	_playerSpriteUpdate();
 	
 	gb.display.print("Level ");
 	gb.display.print(_currentLevel +1);
@@ -16,9 +16,4 @@ void introUpdate() {
 		_state = 2;
 	}
 	
-	if (_player.timer < 4) {
-		gb.display.drawImage(_player.x, _player.y, _spriteHero1);
-	} else {
-		gb.display.drawImage(_player.x, _player.y, _spriteHero2);
-	}
 }
--- variables.h
+++ variables.h
@@ -23,10 +23,16 @@ struct Player {
 Player _player;
 
 
-void _playerTimerUpdate() {
+void _playerSpriteUpdate() {
   _player.timer++;
 	if (_player.timer == 8) {
 		_player.timer = 0;
+	}
+
+  if (_player.timer < 4) {
+		gb.display.drawImage(_player.x, _player.y, _spriteHero1);
+	} else {
+		gb.display.drawImage(_player.x, _player.y, _spriteHero2);
 	}
 }
 
